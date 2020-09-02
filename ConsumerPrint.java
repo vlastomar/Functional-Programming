279ffa0ca6a3f9c560c5a64cc6872dd7465c600e
VSM <vlad24@abv.bg>
2020-09-02 07:23:27 +0300

    Create ConsumerPrint.java

---
+++ ConsumerPrint.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.function.Consumer;
+
+public class ConsumerPrint {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = read.readLine().split("\\s+");
+        Consumer<String> word = w -> System.out.println(w);
+        print(word, input);
+
+    }
+    static void print(Consumer<String> word , String[] input){
+        for (int i = 0; i < input.length ; i++) {
+            word.accept(input[i]);
+        }
+    }
+}
